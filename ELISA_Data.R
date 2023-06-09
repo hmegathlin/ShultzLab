b78410ea4b33abac6023e96cfb8f9b334969cff9
hmegathlin <124177779+hmegathlin@users.noreply.github.com>
2023-06-09 08:43:57 -0400

    Create ELISA_Data.R

---
+++ ELISA_Data.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+
+elisa <- read_delim("~/Desktop/IgG Clearance 2000 Dilution.txt", 
+                    delim = "\t", escape_double = FALSE, 
+                    trim_ws = TRUE)
+
+elisa <- elisa %>% drop_na()           
+colnames(elisa) <- elisa[1,]
+elisa$Dilution <- as.numeric(elisa$Dilution)
+elisa <- elisa %>% drop_na() 
+elisa <- select(elisa, Sample, AdjConc)
+elisa <- separate_wider_delim(elisa, Sample, delim="_", names=c("TimePoint", "Mouse"))
+
+for(i in 1:9){
+  elisa$Mouse[elisa$Mouse==paste0("Mus0", i)] <- paste0("Mus", i)
+}
+
+
+elisa$AdjConc <- as.numeric(elisa$AdjConc)
+elisa$AdjConc <- replace_na(elisa$AdjConc, 0)
+elisa_wider <- pivot_wider(elisa, names_from=TimePoint, values_from=AdjConc, id_cols=Mouse)
+
+
+
+write_csv(elisa_wider, "Avg_AdjConc.csv")
+
+
+
