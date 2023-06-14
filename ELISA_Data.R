707c28944b9aa6fab3801549e567f627eb85d7a4
hmegathlin <124177779+hmegathlin@users.noreply.github.com>
2023-06-14 10:03:00 -0400

    Update ELISA_Data.R

--- ELISA_Data.R
+++ ELISA_Data.R
@@ -1,11 +1,8 @@
 library(tidyverse)
+setwd("~/Desktop/R Working Directory/ELISA")
 
-elisa <- read_delim("~/Desktop/IgG Clearance 2000 Dilution.txt", 
-                    delim = "\t", escape_double = FALSE, 
-                    trim_ws = TRUE)
 
-elisa <- elisa %>% drop_na()           
-colnames(elisa) <- elisa[1,]
+elisa <- elisa %>% drop_na()
 elisa$Dilution <- as.numeric(elisa$Dilution)
 elisa <- elisa %>% drop_na() 
 elisa <- select(elisa, Sample, AdjConc)
@@ -22,9 +19,57 @@ elisa_wider <- pivot_wider(elisa, names_from=TimePoint, values_from=AdjConc, id_
 
 elisa$TimePoint[elisa$TimePoint==T1] <- "24hours"
 
+elisa2000 <- elisa
+
+elisa200$TimePoint <- gsub("TP", "T", elisa200$TimePoint)
+
+write_csv(elisa, "Longer_Elisa.csv")
 
 
 write_csv(elisa_wider, "Avg_AdjConc.csv")
 
 
-ggplot(elisa, aes(x=TimePoint, y=AdjConc)) + geom_point()
+
+##### Standard Curves ####
+Standard200 <- read_csv("Standard200.csv")
+Standard200 <- Standard200 %>% drop_na() 
+Standard200 <- filter(Standard200, Sample<=8)
+
+ggplot() + geom_line(data=Standard200, aes(`Concentration ng/mL`, MeanValue))
+
+
+Standard2000 <- read_csv("Standard2000.csv")
+Standard2000 <- Standard2000 %>% drop_na() 
+Standard2000 <- filter(Standard2000, Sample<=12)
+
+ggplot(Standard2000) + geom_line(aes(`Standard Value ng/mL`, AvgOD))
+
+
+elisa200 <- read_csv("IgG Clearance.csv")
+elisa200 <- elisa200 %>% 
+  select(Sample, OD, AvgConc) %>% 
+  fill(c(Sample, AvgConc), .direction="down")
+elisa200$OD <- as.numeric(elisa200$OD)
+elisa200$AvgConc <- as.numeric(elisa200$AvgConc)
+elisa200 <- elisa200 %>% drop_na()
+elisa200 <- elisa200 %>%  
+  group_by(Sample) %>% 
+  summarise(MeanOD=mean(OD), AvgConc=mean(AvgConc))
+
+elisa2000 <- read_csv("IgG Clearance 2000 Dilution.csv")
+elisa2000 <- elisa2000 %>% 
+  select(Sample, OD, AvgConc) %>% 
+  fill(c(Sample, AvgConc), .direction="down")
+elisa2000$OD <- as.numeric(elisa2000$OD)
+elisa2000$AvgConc <- as.numeric(elisa2000$AvgConc)
+elisa2000 <- elisa2000 %>% drop_na()
+elisa2000 <- elisa2000 %>%  
+  group_by(Sample) %>% 
+  summarise(MeanOD=mean(OD), AvgConc=mean(AvgConc))
+
+d200 <- ggplot() + geom_line(data=Standard200, aes(log(`Concentration ng/mL`), MeanValue)) + geom_point(data=elisa200, aes(log(AvgConc), MeanOD))
+d200
+d2000 <- ggplot() + geom_line(data=Standard2000, aes(log(`Standard Value ng/mL`), AvgOD)) + geom_point(data=elisa2000, aes(log(AvgConc), MeanOD))
+d2000
+
+
